e7123d208bdcbe6d30de48930daf2bb411c26d76
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-26 17:57:43 +0600

    chat routes and message endpoient added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import AuthRoutes from '../app/modules/auth/auth.route';
 import UserRoutes from '../app/modules/client/client.route';
 import ProviderRoutes from '../app/modules/provider/provider.route';
 import NotificationRoutes from '../app/modules/notification/notification.route';
+import ChatRoutes from '../app/modules/chat/chat.route';
+import MessageRoutes from '../app/modules/Message/message.route';
 const router = express.Router();
 
 const apiRoutes = [
@@ -21,6 +23,14 @@ const apiRoutes = [
   {
     path: '/notification',
     route: NotificationRoutes
+  },
+  {
+    path: '/chat',
+    route: ChatRoutes
+  },
+  {
+    path: '/message',
+    route: MessageRoutes
   }
 ];
 
