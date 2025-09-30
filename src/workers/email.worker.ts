91cc9cde158b4d93bd754eedc0dabfdac56dccc0
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-30 15:13:24 +0600

    queue setuped for the email and socket notifications

--- src/workers/email.worker.ts
+++ src/workers/email.worker.ts
@@ -1,16 +1,36 @@
-import { Worker } from "bullmq";
+import { Job, Worker } from "bullmq";
 import { redisConfig } from "../config/redis";
+import Database from './../DB/db';
+import { emailHelper } from "../helpers/emailHelper";
+import { io } from "socket.io-client"
+import config from "../config";
+
+// DB Connection
+;( async () => {
+  await Database.connect();
+  console.log("MongoDB connected successfully");
+})()
+
+// Connect Socket
+const socket = io(`http://${config.ip_address}:${config.port}`);
+socket.on("connect", () => {
+  console.log(`Socket connected on ${config.ip_address}:${config.port}`);
+  socket.emit("email-worker", "Email worker connected with socket id: ", socket.id);
+});
 
 const emailWorker = new Worker(
   "email-queue",
-  async (job) => {
-    console.log(`Processing job ${job.id} with data:`, job.data);
+  async (job: Job) => {
+    console.log(`Processing job ${job.id} with name: ${job.name}`);
 
-    // Example email sending logic
-    const { to, subject, body } = job.data;
-    // await emailHelper.sendEmail({ to, subject, body });
+    if (job.name === "email-send") {
+      await emailHelper.sendEmail(job.data);
+    };
 
-    return { status: "sent", to };
+    if(job.name === "socket-notification") {
+      socket.emit("notification", job.data);
+    };
+    
   },
   { connection: redisConfig }
 );
