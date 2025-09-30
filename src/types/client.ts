644deefa26614b6f70049b6afab50f0dad2b1569
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-30 15:12:46 +0600

    added extra pagination

--- src/types/client.ts
+++ src/types/client.ts
@@ -4,4 +4,7 @@ export type TServicePagination = {
     servicesLimit: number;
     servicesPage: number;
     servicesSortOrder: "asc" | "desc";
+    reviewLimit: number;
+    reviewPage: number;
+    reviewSortOrder: "asc" | "desc";
 }
