d56e62e5df84ed6e49e00bfff6564c4b009cdcc8
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-01 18:22:44 +0600

    requeried thinks removed

--- src/app/modules/payment/payment.model.ts
+++ src/app/modules/payment/payment.model.ts
@@ -3,12 +3,12 @@ import { IPayment } from "./payment.interface";
 import { PAYMENT_STATUS } from "../../../enums/payment";
 
 const paymentSchema = new Schema<IPayment>({
-  customer: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  provider: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  service: { type: Schema.Types.ObjectId, ref: "Service", required: true },
-  booking: { type: Schema.Types.ObjectId, ref: "Booking", required: true },
+  customer: { type: Schema.Types.ObjectId, ref: "User"},
+  provider: { type: Schema.Types.ObjectId, ref: "User" },
+  service: { type: Schema.Types.ObjectId, ref: "Service" },
+  booking: { type: Schema.Types.ObjectId, ref: "Booking" },
   amount: { type: Number, required: true },
-  paymentId: { type: String, required: true },
+  paymentId: { type: String },
   paymentStatus: { type: String, required: true, enum: PAYMENT_STATUS },
 }, { timestamps: true });
 
