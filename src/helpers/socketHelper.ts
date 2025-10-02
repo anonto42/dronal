8613eaebc834d54ada4c9fd6436fbda692c75150
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-02 15:41:18 +0600

    message forword added

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -3,6 +3,8 @@ import { Server } from 'socket.io';
 import { logger } from '../shared/logger';
 import { redisDB } from '../redis/connectedUsers';
 import { INotification } from '../app/modules/notification/notification.interface';
+import { IMessage } from '../app/modules/Message/message.interface';
+import { IChat } from '../app/modules/chat/chat.interface';
 
 const socket = (io: Server) => {
   io.on('connection', socket => {
@@ -13,11 +15,19 @@ const socket = (io: Server) => {
       await redisDB.set(`user:${userId}`, socket.id);
     })
 
-    // Send notification to provider
+    // Send notification 
     socket.on("notification", async (data: INotification) => {
       const userId = data.for;
       const socketId = await redisDB.get(`user:${userId}`);
       socket.to(socketId!).emit("notification", data.message)
+    })
+
+    // Send message 
+    socket.on("message", async (data: { message: IMessage, chat: IChat }) => {
+      data.chat.participants.forEach(async element => {
+        const socketId = await redisDB.get(`user:${element}`);
+        socket.to(socketId!).emit("message", data.message)
+      });
     })
 
     //disconnect
