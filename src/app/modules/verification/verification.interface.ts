ae4ff7b97bcb05ed2a04a7adae48da059e24a039
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-02 10:34:01 +0600

    verifination added

--- src/app/modules/verification/verification.interface.ts
+++ src/app/modules/verification/verification.interface.ts
@@ -3,6 +3,7 @@ import { VERIFICATION_STATUS } from "../../../enums/user"
 
 
 export interface IVerificaiton {
+    _id?: Types.ObjectId,
     user: Types.ObjectId,
     status: VERIFICATION_STATUS,
     nid: string,
