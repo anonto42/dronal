cb771e87d0b08bc2468599de46898585aa585b85
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-23 12:38:33 +0600

    update notification and get with the status

--- src/app/modules/notification/notification.service.ts
+++ src/app/modules/notification/notification.service.ts
@@ -3,6 +3,7 @@ import mongoose, { Types } from "mongoose";
 import { IPaginationOptions } from "../../../types/pagination";
 import { JwtPayload } from "jsonwebtoken";
 import { buildPaginationResponse } from "../../../util/pagination";
+import { Notification } from "./notification.model";
 
 export class NotificationService {
   private notificationRepo: NotificationRepository;
@@ -13,14 +14,29 @@ export class NotificationService {
 
   public async getMany(
     payload: JwtPayload,
-    paginationOptions: IPaginationOptions
+    paginationOptions: IPaginationOptions & { status: "read" | "unRead" | null }
   ): Promise<any> {
     const data = await this.notificationRepo.findMany({
-      filter:{ for: new mongoose.Types.ObjectId( payload.id )},
+      filter:{
+        for: new mongoose.Types.ObjectId(payload.id),//@ts-ignore
+        isRead: paginationOptions.status 
+          ? paginationOptions.status === "read" 
+            ? true 
+            : paginationOptions.status === "unRead" 
+            ? false 
+            : { $ne: null }
+          : { $ne: null }
+      },
       paginationOptions,
       select: "-for -updatedAt"
     });
-    const coutuntDocument = await this.notificationRepo.countDocument(payload.id);
+    const coutuntDocument = await Notification.countDocuments({for: payload.id, isRead: paginationOptions.status 
+          ? paginationOptions.status === "read" 
+            ? true 
+            : paginationOptions.status === "unRead" 
+            ? false 
+            : { $ne: null }
+          : { $ne: null }});
 
     return buildPaginationResponse(data,coutuntDocument,paginationOptions.page!, paginationOptions.limit!)
   }
@@ -31,11 +47,13 @@ export class NotificationService {
   }
 
   public async markAllAsRead(
-    ids: string[]
+    ids: string[],
+    payload: JwtPayload
   ) {
     const objectIds = ids.map(id => new Types.ObjectId(id));
+    if( objectIds.length <= 0 ) await this.notificationRepo.updateMany({for: payload.id},{ isRead: true, readAt: new Date( Date.now())});
 
-    return this.notificationRepo.updateManyByIds(
+    return await this.notificationRepo.updateManyByIds(
       objectIds,
       { isRead: true, readAt: new Date() }
     );
